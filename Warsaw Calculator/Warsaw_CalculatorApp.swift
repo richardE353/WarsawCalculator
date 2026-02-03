3fc30d35d9f3f8e3204cb2a267ca67adbd5e7a5a
Richard Austin <richard.e353@gmail.com>
2026-02-02 16:03:36 -0800

    Initial Commit

---
+++ Warsaw Calculator/Warsaw_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Warsaw_CalculatorApp.swift
+//  Warsaw Calculator
+//
+//  Created by Richard Austin on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Warsaw_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Warsaw CalculatorTests/Warsaw_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Warsaw_CalculatorTests.swift
+//  Warsaw CalculatorTests
+//
+//  Created by Richard Austin on 2/2/26.
+//
+
+import Testing
+@testable import Warsaw_Calculator
+
+struct Warsaw_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
